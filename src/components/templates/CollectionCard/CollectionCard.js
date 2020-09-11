b5d2ed83e910070762dc103f8c8b24991456b0f9
Sudipto Roy <sudipto.roy@naukri.com>
2020-09-11 16:53:22 +0530

    styling of the cta messafge

--- src/components/templates/CollectionCard/CollectionCard.js
+++ src/components/templates/CollectionCard/CollectionCard.js
@@ -1,48 +1,54 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import {getExpStr, getPackageStr} from "../../../utils/jobDetailsUtils";
+import { getExpStr, getPackageStr } from "../../../utils/jobDetailsUtils";
 import getKey from "../../../utils/getKey";
 import SpanWithIcon from "../../molecules/SpanWithIcon";
 import "./CollectionCard.scss";
 import Button from "../../../ui-components/Button";
-import {CTA_TYPE, findCTA} from "./util";
+import { CTA_TYPE, findCTA } from "./util";
 
-const CollectionCard = ({ jobDetails, companyName, applyOnJob, renderIndex, history }) => {
-    const [loading, setLoading] = useState(false);
-    const primaryCTA = findCTA(jobDetails);
+const CollectionCard = ({
+  jobDetails,
+  companyName,
+  applyOnJob,
+  renderIndex,
+  history
+}) => {
+  const [loading, setLoading] = useState(false);
+  const primaryCTA = findCTA(jobDetails);
 
-    const handlePrimaryCTA = () => {
-        const ctaValue = primaryCTA.value
-        switch (ctaValue) {
-            case CTA_TYPE.APPLY.value:
-                setLoading(true);
-                applyOnJob(jobDetails.jobId, renderIndex, setLoading)
-                break;
+  const handlePrimaryCTA = () => {
+    const ctaValue = primaryCTA.value;
+    switch (ctaValue) {
+      case CTA_TYPE.APPLY.value:
+        setLoading(true);
+        applyOnJob(jobDetails.jobId, renderIndex, setLoading);
+        break;
 
-            case CTA_TYPE.ASSESSMENT.value:
-                break;
+      case CTA_TYPE.ASSESSMENT.value:
+        break;
 
-            case CTA_TYPE.INTERVIEW.value:
-                history.push(`#/${jobDetails.interviewRoomId}`);
-                location.reload();
-                break;
-            default:
-                return null;
-        }
+      case CTA_TYPE.INTERVIEW.value:
+        history.push(`#/${jobDetails.interviewRoomId}`);
+        location.reload();
+        break;
+      default:
+        return null;
     }
+  };
 
   return (
-      <div className="CollectionCard">
-          <div className="card-details">
-            <div className="logoWrapper">
-              <img
-                className="companyLogo"
-                src={jobDetails.companyLogoUrl}
-                alt="company logo"
-              />
-            </div>
+    <div className="CollectionCard">
+      <div className="card-details">
+        <div className="logoWrapper">
+          <img
+            className="companyLogo"
+            src={jobDetails.companyLogoUrl}
+            alt="company logo"
+          />
+        </div>
 
-            <div className="cardContent">
+        <div className="cardContent">
           <h4>{jobDetails.designation}</h4>
           <h5>{companyName}</h5>
           <div className="jobDetails">
@@ -88,42 +94,44 @@ const CollectionCard = ({ jobDetails, companyName, applyOnJob, renderIndex, hist
             </div>
           )}
         </div>
-          </div>
-        <div className="action-btn-grp">
-            <Button
-                className=""
-                onClick={() => {
-                    const winRef = window.open(`${location.pathname}/${jobDetails.jobId}`);
-                    jobDetails.companyName = companyName;
-                    winRef.data = jobDetails;
-                }}
-                appearance="secondary"
-                type="button"
-                size="small"
-            >
-                Know More
-            </Button>
-            {primaryCTA.label &&
-                <Button
-                    className=""
-                    loading={loading}
-                    onClick={handlePrimaryCTA}
-                    appearance="primary"
-                    type="button"
-                    size="small"
-                >
-                    {primaryCTA.label}
-                </Button>
-            }
-        </div>
-          {primaryCTA.msg && (<span>{primaryCTA.msg}</span>)}
       </div>
+      <div className="action-btn-grp">
+        <Button
+          className=""
+          onClick={() => {
+            const winRef = window.open(
+              `${location.pathname}/${jobDetails.jobId}`
+            );
+            jobDetails.companyName = companyName;
+            winRef.data = jobDetails;
+          }}
+          appearance="secondary"
+          type="button"
+          size="small"
+        >
+          Know More
+        </Button>
+        {primaryCTA.label && (
+          <Button
+            className=""
+            loading={loading}
+            onClick={handlePrimaryCTA}
+            appearance="primary"
+            type="button"
+            size="small"
+          >
+            {primaryCTA.label}
+          </Button>
+        )}
+      </div>
+      {primaryCTA.msg && <span className="ctaMsg">{primaryCTA.msg}</span>}
+    </div>
   );
 };
 
 CollectionCard.propTypes = {
   queryParams: PropTypes.string,
-  jobDetails: PropTypes.object.isRequired,
+  jobDetails: PropTypes.object.isRequired
 };
 
 CollectionCard.defaultProps = {
