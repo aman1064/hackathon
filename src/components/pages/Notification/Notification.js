ed88416cd2d0dfefe28f4b28ce570de1a0ba0ab9
Yatindra Khandal <yatindra.khandal@naukri.com>
2020-09-11 23:58:18 +0530

    header navbar in notic-board

--- src/components/pages/Notification/Notification.js
+++ src/components/pages/Notification/Notification.js
@@ -2,8 +2,12 @@ import React, {useEffect} from "react";
 import { connect } from "react-redux";
 import "./notification.scss";
 import {getNotifications} from "../../../sagas/ActionCreator";
+import LogoHeader from "../../organisms/LogoHeader";
+import routeConfig from "../../../constants/routeConfig";
+import {Link} from "react-router-dom";
+import Username from "../../templates/Username";
 
-const Notification = ({getNotifications, userId, notificationHistory}) => {
+const Notification = ({getNotifications, userId, notificationHistory, history, userName}) => {
     useEffect(() => {
         getNotifications(userId)
     }, [userId]);
@@ -14,6 +18,19 @@ const Notification = ({getNotifications, userId, notificationHistory}) => {
 
     return (
         <div className="notification-container">
+            <div className="LogoHeaderCntnr">
+                <LogoHeader redirectLink={routeConfig.home}>
+                    <div className="LogoHeaderLinks">
+                        <div className="linkItem">
+                            <Link to={routeConfig.home}>Job Fair</Link>
+                        </div>
+                        <div className="linkItem">
+                            <Link to={routeConfig.exibitorFloor}>Exibitor Floor</Link>
+                        </div>
+                        <Username history={history} userName={userName} isLoggedIn />
+                    </div>
+                </LogoHeader>
+            </div>
             {
                 Object.keys(notificationHistory).map((key) => {
                     return <div key={key} className={key}>
@@ -39,7 +56,8 @@ const Notification = ({getNotifications, userId, notificationHistory}) => {
 
 const mSTP = ({commonData}) => ({
     userId: commonData.userBasicDetails.id,
-    notificationHistory: commonData.notifications
+    notificationHistory: commonData.notifications,
+    userName: commonData.userDetails.name
 });
 
 const mDTP = {
